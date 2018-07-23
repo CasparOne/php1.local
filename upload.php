8a96c7398aae4b23422b3099a412b379ee560a64
CasparOne <vvolodin.victor@gmail.com>
2018-07-23 15:56:22 +0300

    Добавлен функционал галереи

--- upload.php
+++ upload.php
@@ -1,10 +1,13 @@
 <?php
+/** проверяем был ли передан файл отпользователя */
 if (isset($_FILES['myimage'])){
     $myimg = $_FILES['myimage'];
     $name = $myimg['name'];
     $size = $myimg['size'];
     $type = $myimg['type'];
     $err = $myimg['error'];
+    /** Проверяем наличие ошибок, соответствие размеру файла. типу файла.
+     * Переносим загруженный файл в нужное нам место*/
     if (0 == $err &&
         1048576 >= $size &&
         ('image/jpeg' == $type ||
