f917383bab9d0dbb96c29b730ed23890d2aedc2f
CasparOne <vvolodin.victor@gmail.com>
2018-07-23 15:02:15 +0300

    Гостевая книга. Коментарии добавлены

--- gustbook.php
+++ gustbook.php
@@ -3,7 +3,11 @@ include __DIR__ . '/functions.php';
 $dataFile = __DIR__ . '/data.txt';
 $records = getRecord();
 $newRecord = $_POST['newRecord'];
-
+/** Проверяем пустая ли строка и если есть пробелы убираем в начале и в конце строки
+ * Строку добавляем в массив записей
+ * массв записей приводим в строку
+ * Записываем в файл
+ * после окончания работы скрипта перенаправляем на главную*/
 if ('' != $newRecord) {
 $records[] = trim($newRecord);
 file_put_contents($dataFile,implode("\n", $records));
