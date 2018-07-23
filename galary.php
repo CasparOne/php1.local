93612f455d3576e09a1b21f0df076f739f5f0ee5
CasparOne <vvolodin.victor@gmail.com>
2018-07-23 13:12:17 +0300

    Добавлен функционал галереии

---
+++ galary.php
@@ -0,0 +1,22 @@
+<?php
+$imgPath = __DIR__ . '/images';
+$imgList = array_diff(scandir($imgPath), ['..', '.']);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Галерея</title>
+</head>
+<body>
+<h1>Галерея</h1>
+<?php foreach ($imgList as $key => $value) {
+    ?><a href="/image.php?id=<?echo $key; ?>"><img src="/images/<?php echo $value?>" height="180">
+<?php
+}
+?>
+</body>
+</html>
---
+++ image.php
@@ -0,0 +1,23 @@
+<?php
+$imgPath = __DIR__ . '/images';
+$imgList = array_diff(scandir($imgPath), ['..', '.']);
+$imgId = $_GET['id'];
+if (isset($imgId) && key_exists($imgId, $imgList)) {
+    $img = $imgList[$imgId];
+} else {
+    die();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<img src="/images/<?php echo $img; ?>">
+</body>
+</html>
