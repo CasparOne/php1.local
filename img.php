277e25933b5fb694f785d87d5b52b62cb54a1c1e
CasparOne <vvolodin.victor@gmail.com>
2018-07-25 09:59:02 +0300

    Добавлен класс View

--- img.php
+++ img.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (empty($_SESSION['usr'])) {
+    header('Location:http://php1.local/login.php');
+}
 if (isset($_GET['id'])) {
     require __DIR__ . '/classes/Images.php';
     $images = new Images();
