f1895228d05a54504d3fe10f4ad41d23b3b2fd04
CasparOne <vvolodin.victor@gmail.com>
2018-07-25 10:37:31 +0300

    Добавлен класс View

--- classes/View.php
+++ classes/View.php
@@ -5,16 +5,12 @@ class View
     protected $data = [];
     public function assign($name, $value)
     {
-        $this->data[$name] = $value;
+
     }
 
     public function display($template)
     {
-        include __DIR__ . '/../templates/' . $template;
-        ob_start();
-        $data = ob_get_contents();
-        ob_end_clean();
-        return $data;
+
     }
 
     public function render($template)
