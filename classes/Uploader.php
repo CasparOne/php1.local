ae350ef0f8652607b09a2e636890f46ac19619ab
CasparOne <vvolodin.victor@gmail.com>
2018-08-14 12:10:06 +0300

    Исправление ошибок. Добавил пробелы и переносы строк для читаемости кода Поправил класс Uploader. Реализовал проверки и запись логов в файл

--- classes/Uploader.php
+++ classes/Uploader.php
@@ -20,7 +20,23 @@ class Uploader
      */
     public function isUploaded()
     {
-        return is_uploaded_file($this->myimg['tmp_name']);
+        if (isset($this->myimg['tmp_name']) && !empty($this->myimg['tmp_name']) &&
+            (0 == $this->myimg['error']) &&
+            is_uploaded_file($this->myimg['tmp_name']) &&
+            (
+                /* Можно реализовать с помощью защищенного свойства типа Array, но для наглядности сделал так*/
+                'image/jpeg' === $this->myimg['type'] ||
+                'image/gif' === $this->myimg['type'] ||
+                'image/gif' === $this->myimg['type'] ||
+                'image/png' === $this->myimg['type']
+            ) &&
+            1048576 >= $this->myimg['size '] ) {
+            return true;
+
+        } else {
+            return false;
+
+        }
     }
 
     /**
@@ -28,18 +44,11 @@ class Uploader
      */
     public function upload()
     {
-        if (isset($this->myimg['tmp_name']) && !empty($this->myimg['tmp_name']) &&
-            (0 == $this->myimg['error']) &&
-            $this->isUploaded() &&
-            (
-             'image/jpeg' === $this->myimg['type'] ||
-             'image/gif' === $this->myimg['type'] ||
-             'image/gif' === $this->myimg['type'] ||
-             'image/png' === $this->myimg['type']
-            ) &&
-             1048576 >= $this->myimg['size '] ) {
+        if ( $this->isUploaded() ) {
             move_uploaded_file($this->myimg['tmp_name'], __DIR__ . '/../images/' . $this->myimg['name']);
 
+            /* можно вынести данную проверку в отдельный метод. например, saveLog(). И вызывать этот метод после upload()
+            */
             if (is_writable( __DIR__ . '/../upload.log')) {
                 $log = date('Y-m-d-H-i-s') . ':' . $_SESSION['usr'] . ':' . $this->myimg['name'];
                 $logFile = file(__DIR__ . '/../upload.log', FILE_IGNORE_NEW_LINES);
@@ -50,6 +59,11 @@ class Uploader
                 }
                 file_put_contents(__DIR__ . '/../upload.log', $record);
             }
+
+            return true;
+        } else {
+
+            return false;
         }
     }
 
