35a3055271c470cd65396c507d86924db45dffac
CasparOne <vvolodin.victor@gmail.com>
2018-08-14 11:41:09 +0300

    Исправление ошибок. Добавил пробелы и переносы строк для читаемости кода Поправил класс Uploader. Реализовал проверки и запись логов в файл

--- classes/Uploader.php
+++ classes/Uploader.php
@@ -16,15 +16,11 @@ class Uploader
     }
 
     /**
-     * @return $this|bool
+     * @return bool
      */
     public function isUploaded()
     {
-        if (is_uploaded_file($_FILES[$this->formName]['tmp_name']) ) {
-          return true;
-        } else {
-            return false;
-        }
+        return is_uploaded_file($this->myimg['tmp_name']);
     }
 
     /**
@@ -32,8 +28,26 @@ class Uploader
      */
     public function upload()
     {
-        if ($this->isUploaded()) {
+        if ( $this->isUploaded() &&
+            (
+             'image/jpeg' === $this->myimg['type'] ||
+             'image/gif' === $this->myimg['type'] ||
+             'image/gif' === $this->myimg['type'] ||
+             'image/png' === $this->myimg['type']
+            ) &&
+             1048576 >= $this->myimg['size '] ) {
             move_uploaded_file($this->myimg['tmp_name'], __DIR__ . '/../images/' . $this->myimg['name']);
+
+            if (is_writable( __DIR__ . '/../upload.log')) {
+                $log = date('Y-m-d-H-i-s') . ':' . $_SESSION['usr'] . ':' . $this->myimg['name'];
+                $logFile = file(__DIR__ . '/../upload.log', FILE_IGNORE_NEW_LINES);
+                $logFile[] = $log;
+
+                foreach ($logFile as $line) {
+                    $record = implode("\n", $logFile);
+                }
+                file_put_contents(__DIR__ . '/../upload.log', $record);
+            }
         }
     }
 
